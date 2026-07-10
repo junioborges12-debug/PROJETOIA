3b4b34d37503126af54837d1c8abd9fda2cb3012
junioborges12-debug <junioborges12@gmail.com>
2026-07-10 19:02:58 -0300

    feat: atualiza service-worker.js para editais diários

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE = 'projetoia-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+const CACHE = 'projetoia-v2';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './data/editais.json'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
